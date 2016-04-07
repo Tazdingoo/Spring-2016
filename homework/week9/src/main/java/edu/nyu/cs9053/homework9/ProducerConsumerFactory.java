9c03a9d3202230dc57c72b59f1a0f3699c823831
blangel <langelb@gmail.com>
2016-04-06 20:13:10 -0400

    * Adding week 9 homework

---
+++ homework/week9/src/main/java/edu/nyu/cs9053/homework9/ProducerConsumerFactory.java
@@ -0,0 +1,20 @@
+package edu.nyu.cs9053.homework9;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * User: blangel
+ * Date: 4/3/16
+ * Time: 2:18 PM
+ */
+public class ProducerConsumerFactory {
+
+    public static Producer createProducer() {
+	// TODO
+    }
+
+    public static Consumer createConsumer() {
+	// TODO
+    }
+
+}
